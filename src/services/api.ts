3e25f766ac77c43205853126bc944dd9a4a4bc98
Michael Lopes Bastos <michaellb17@gmail.com>
2026-08-06 00:25:07 -0300

    Commit de para o render

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 export const api = axios.create({
   // URL do nosso backend local (quando formos para a nuvem, mudaremos apenas aqui)
-  baseURL: 'http://localhost:3333', 
+  baseURL: 'https://finances-api-jyfj.onrender.com', 
 });
